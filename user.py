71c442e820315370e287287102d71e9a2458be52
liuchang0812 <liuchang0812@gmail.com>
2014-09-02 01:20:55 +0800

    update user.py
    Get/Set/Update method

--- user.py
+++ user.py
@@ -1,12 +1,58 @@
 # -*- encoding=utf-8 -*-
+from __future__ import print_function
+import unittest
 
 class User(object):
     '''The base class of user
 
     '''
-    def __init__(self, name, email, nickname):
-        self._name = name 
+    def __init__(self, name, email, nickname="newbee"):
+        if not name:
+            raise ValueError("name can't be None")
+        self._name = name
+        if not email:
+            raise ValueError("email can't be None")
         self._email = email
         self._nickname = nickname
 
+    def Update(self, name, email, nickname):
 
+        if name:
+            self._name = name
+
+        if email:
+            self._email = email
+
+        if nickname:
+            self._nickname = nickname
+            
+    def Get(self, property):
+        try:
+            return getattr(self, "_" + property.lower())
+        except AttributeError as e:
+            print(str(e))
+            return None
+
+    def Set(self, property, value):
+        try:
+            return setattr(self, "_" + property.lower(),value)
+        except AttributeError as e:
+            print(str(e))
+            return None
+
+
+class UserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.user = User(name="liuchang", email="liuchang0812@gmail.com", nickname="chenchu")
+
+    def test_Get(self):
+        self.assertEqual(self.user.Get("name"), "liuchang")
+
+    def test_Set(self):
+        self.user.Set("nickname", "lc")
+        self.assertEqual("lc", self.user._nickname)
+
+
+if __name__ == "__main__":
+    unittest.main()
